d91acd6453aecaac996bddfbc69cdc2584cc0e56
HARISHANKAR R <rharihumix@gmail.com>
2026-08-22 22:33:52 +0530

    Synchronize existing submission history (1/1)

---
+++ array/longest-common-prefix.py
@@ -0,0 +1,9 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        n = len(strs)
+        for i in range(len(strs[0])):
+            for j in range(1, n):
+                if i >= len(strs[j]) or strs[0][i] != strs[j][i]:
+                    return strs[0][:i]
+
+        return strs[0]
