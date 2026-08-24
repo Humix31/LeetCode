cfcfe09959a1c05c3ab20cacbcdb2c4d67bccbe5
HARISHANKAR R <rharihumix@gmail.com>
2026-08-24 19:27:39 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- array/3sum.java
+++ array/3sum.java
@@ -1,22 +1,40 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+
         Arrays.sort(nums);
+
         List<List<Integer>> result = new ArrayList<>();
-        for (int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                for(int k=j+1;k<nums.length;k++){
-                    if (nums[i]+nums[j]+nums[k]==0){
-                       List<Integer> triplet = Arrays.asList(
-                        nums[i], nums[j], nums[k]
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // Skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < nums.length - 1; j++) {
+
+                // Skip duplicate j
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+
+                for (int k = j + 1; k < nums.length; k++) {
+
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+
+                        List<Integer> triplet = Arrays.asList(
+                            nums[i],
+                            nums[j],
+                            nums[k]
                         );
-                        Collections.sort(triplet);
-                        if (!result.contains(triplet)) {
-                            result.add(triplet);
-                            }
-                     }
+
+                        result.add(triplet);
+                    }
                 }
             }
         }
-    return result;
-   }
+
+        return result;
+    }
 }
