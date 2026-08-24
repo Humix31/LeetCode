2e0d0ad4f25ad983c1c7345211223f3192af966f
HARISHANKAR R <rharihumix@gmail.com>
2026-08-24 19:31:37 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- two-pointers/3sum.java
+++ two-pointers/3sum.java
@@ -1,45 +1,30 @@
 class Solution {
+    
     public List<List<Integer>> threeSum(int[] nums) {
-
         Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+       int n = nums.length;
+       for(int i = 0;i < n - 2;i++){
+        int j = i+1,k = n - 1;
+        
+        if(i > 0 && nums[i] == nums[i-1]) continue;
+        
+        while(j < k){
+            int sum = nums[i] + nums[j] + nums[k];
+            if(sum == 0){
+            res.add(Arrays.asList(nums[i],nums[j],nums[k]));
+            j++;
+            k--;
+            while(j < k && nums[j] == nums[j-1]) j++;
+            }else if(sum < 0) j++;
+            else k--;
 
-        List<List<Integer>> result = new ArrayList<>();
-
-        for (int i = 0; i < nums.length - 2; i++) {
-
-            // Skip duplicate first numbers
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            for (int j = i + 1; j < nums.length - 1; j++) {
-
-                // Skip duplicate second numbers
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
-                }
-
-                for (int k = j + 1; k < nums.length; k++) {
-
-                    // Skip duplicate third numbers
-                    if (k > j + 1 && nums[k] == nums[k - 1]) {
-                        continue;
-                    }
-
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-
-                        List<Integer> triplet = Arrays.asList(
-                            nums[i],
-                            nums[j],
-                            nums[k]
-                        );
-
-                        result.add(triplet);
-                    }
-                }
-            }
         }
+       }
 
-        return result;
+       return res;
+
+
+
     }
 }
