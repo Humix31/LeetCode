60e4d453fef770b0c8c4a8b95d873b661ab48879
HARISHANKAR R <rharihumix@gmail.com>
2026-08-24 19:21:33 +0530

    LC-3sum [Runtime: N/A; Memory: N/A]

--- two-pointers/3sum.java
+++ two-pointers/3sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
         for (int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
